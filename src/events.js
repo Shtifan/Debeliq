5ad457083f1cdc197c7811984e3b25e31a9951bd
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2023-12-20 20:04:49 +0200

    moved cb to slash games

--- src/events.js
+++ src/events.js
@@ -1,39 +1,6 @@
 const client = require('./main.js');
-
-const prefix = 'debel ';
-let gamecb = false;
-
-function hasDuplicates(array) {
-    return new Set(array).size !== array.length;
-}
-
-function cb(number, input) {
-    let bulls = 0;
-    let cows = 0;
-    for (i = 0; i < number.length; i++) {
-        if (number[i] == input[i]) bulls++;
-        else if (number.indexOf(input[i]) != -1) cows++;
-    }
-    return `${bulls} bull${bulls != 1 ? 's' : ''} ${cows} cow${cows != 1 ? 's' : ''}`;
-}
-
-function getNumberEnding(number) {
-    const lastDigit = number % 10;
-    const lastTwoDigits = number % 100;
-
-    switch (lastDigit) {
-        case 1:
-            if (lastTwoDigits != 11) return 'st';
-            break;
-        case 2:
-            if (lastTwoDigits != 12) return 'nd';
-            break;
-        case 3:
-            if (lastTwoDigits != 13) return 'rd';
-            break;
-    }
-    return 'th';
-}
+const { useMainPlayer } = require('discord-player');
+const player = useMainPlayer();
 
 client.on('messageCreate', async message => {
     if (message.author.bot) return;
@@ -41,40 +8,7 @@ client.on('messageCreate', async message => {
     if (message.content.includes('koi') || message.content.includes('koj')) {
         message.reply('te e pital');
     }
-
-    if (message.content === `${prefix}cb`) {
-        gamecb = true;
-        guesses = 0;
-        number = '';
-        let firstDigit = Math.floor(Math.random() * 9) + 1;
-        number += firstDigit;
-        for (let i = 0; i < 3; i++) {
-            let digit = Math.floor(Math.random() * 10);
-            while (number.includes(digit.toString()) || (i === 0 && digit === 0)) {
-                digit = Math.floor(Math.random() * 10);
-            }
-            number += digit.toString();
-        }
-        number.split('').map(Number);
-        message.channel.send("I'm ready");
-    }
-
-    if (gamecb) {
-        let input = message.content.split('').map(Number);
-        if (isNaN(message.content) || input.length != 4 || hasDuplicates(input)) return;
-        guesses++;
-
-        message.channel.send(cb(number, input));
-        if (cb(number, input) === '4 bulls 0 cows') {
-            message.channel.send(`Well done, you guessed the number from the ${guesses + getNumberEnding(guesses)} guess`);
-            gamecb = false;
-            guesses = 0;
-        }
-    }
 });
-
-const { useMainPlayer } = require('discord-player');
-const player = useMainPlayer();
 
 player.events.on('error', (queue, error) => {
     console.log(`General player error event: ${error.message}`);
