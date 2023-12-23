a1516dca3f7dd19a67821320021e86d178db5a7f
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2023-12-23 13:26:29 +0200

    v1 dd

--- src/commands/games/deal.js
+++ src/commands/games/deal.js
@@ -75,11 +75,6 @@ function remainingNumbers(cases) {
     return `Remaining numbers: ${numbers}`;
 }
 
-function swapCase(yourCase, finalCase) {
-    let remainingCases = cases.filter(_case => _case.number != yourCase);
-    return remainingCases.length > 0 ? remainingCases[0].number : finalCase;
-}
-
 let gamedeal = false;
 let yourCase = 0;
 
@@ -87,16 +82,14 @@ module.exports = {
     data: new SlashCommandBuilder().setName('deal').setDescription('Play deal or no deal'),
 
     async execute(interaction) {
-        interaction.deferReply();
-        interaction.deleteReply();
         gamedeal = true;
-        interaction.channel.send('The deal or no deal game has started.');
-        interaction.channel.send('These are all the briefcases:');
+        await interaction.reply('The deal or no deal game has started.');
+        await interaction.channel.send('These are all the briefcases:');
         let remaining = remainingCases(cases);
-        interaction.channel.send(remaining);
+        await interaction.channel.send(remaining);
         shuffle(cases);
-        interaction.channel.send('The briefcases have been shuffled.');
-        interaction.channel.send('Choose your briefcase:');
+        await interaction.channel.send('The briefcases have been shuffled.');
+        await interaction.channel.send('Choose your briefcase:');
     },
 };
 
@@ -104,61 +97,20 @@ client.on('messageCreate', async message => {
     if (message.author.bot) return;
     if (!gamedeal) return;
 
-    let input = parseInt(message.content);
-    if (input < 1 || input > 26) return;
-
-    let index = 0;
-    if (cases.findIndex(_case => _case.number == input) == -1) return;
-    else index = cases.findIndex(_case => _case.number == input);
-
-    if (input == yourCase) return;
-
-    if (yourCase == 0) {
-        yourCase = input;
-        message.channel.send('Now choose 6 briefcases to reveal:');
-    } else {
-        remove(input, cases);
-        message.channel.send(`Behind case ${input} there were $${cases[index].value}`);
-
-        let special = [20, 15, 11, 8, 5, 2];
-        if (special.includes(cases.length)) {
-            message.channel.send(`The banker offer is $${getOffer(cases)}`);
-            message.channel.send('Do you accept the deal?');
-        }
-
-        if (cases.length == 1) message.channel.send('Do you want to switch your case with the last remaining?');
-
-        if (!(special.includes(cases.length) || cases.length == 1)) {
-            let remaining = remainingCases(cases);
-            message.channel.send(remaining);
-            let numbers = remainingNumbers(cases);
-            message.channel.send(numbers);
-        }
-    }
-});
-
-client.on('messageCreate', async message => {
-    if (message.author.bot) return;
-    if (!gamedeal) return;
-
-    let input = message.content;
-
-    if (input.toLowerCase() == 'yes') {
+    if (message.content.toLowerCase() == 'yes') {
         if (cases.length == 1) {
-            let finalCase = swapCase(yourCase, cases[0].number);
+            let finalCase = cases[0].number;
             let index = cases.findIndex(_case => _case.number == finalCase);
-            let value = cases[index].value;
-            message.channel.send(`Congratulations! You win $${value}`);
+            message.channel.send(`Congratulations! You win $${cases[index].value}`);
             gamedeal = false;
         } else {
             message.channel.send(`Congratulations! You win $${getOffer(cases)}`);
             gamedeal = false;
         }
-    } else if (input.toLowerCase() == 'no') {
+    } else if (message.content.toLowerCase() == 'no') {
         if (cases.length == 1) {
             let index = cases.findIndex(_case => _case.number == yourCase);
-            let value = cases[index].value;
-            message.channel.send(`Congratulations! You win $${value}`);
+            message.channel.send(`Congratulations! You win $${cases[index].value}`);
             gamedeal = false;
         } else {
             let special = [20, 15, 11, 8, 5, 2];
@@ -168,6 +120,38 @@ client.on('messageCreate', async message => {
             } else {
                 message.channel.send('You declined the dealer offer');
                 gamedeal = false;
+            }
+        }
+    } else {
+        let input = parseInt(message.content);
+        if (isNaN(message.content) || input < 1 || input > 26) return;
+
+        let index = 0;
+        if (cases.findIndex(_case => _case.number == input) == -1) return;
+        else index = cases.findIndex(_case => _case.number == input);
+
+        if (input == yourCase) return;
+
+        if (yourCase == 0) {
+            yourCase = input;
+            message.channel.send('Now choose 6 briefcases to reveal:');
+        } else {
+            message.channel.send(`Behind case ${input} there were $${cases[index].value}`);
+            remove(input, cases);
+
+            let special = [20, 15, 11, 8, 5, 2];
+            if (special.includes(cases.length)) {
+                message.channel.send(`The banker offer is $${getOffer(cases)}`);
+                message.channel.send('Do you accept the deal?');
+            }
+
+            if (cases.length == 1) message.channel.send('Do you want to switch your case with the last remaining?');
+
+            if (!(special.includes(cases.length) || cases.length == 1)) {
+                let remaining = remainingCases(cases);
+                message.channel.send(remaining);
+                let numbers = remainingNumbers(cases);
+                message.channel.send(numbers);
             }
         }
     }
