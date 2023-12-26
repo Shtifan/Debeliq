636c92f92d9f4b4b1b1ce1ca6a948998286671be
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2023-12-26 18:42:33 +0200

    bug fix

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -5,37 +5,30 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
         .setDescription('Plays a song')
-        .addStringOption(option => option.setName('song').setDescription('Name or URL of the song').setRequired(true)),
+        .addStringOption(option => option.setName('query').setDescription('Name or URL of the song').setRequired(true)),
 
     async execute(interaction) {
         const player = useMainPlayer();
-
         const channel = interaction.member.voice.channel;
         if (!channel)
             return interaction.reply({
                 content: 'You are not connected to a voice channel',
                 ephemeral: true,
             });
-
-        const query = interaction.options.getString('song', true);
-        const searchResult = await player.search(query, {
-            requestedBy: interaction.user,
-        });
+        const query = interaction.options.getString('query', true);
 
         await interaction.deferReply();
-        if (!searchResult.hasTracks()) {
-            return interaction.editReply({
-                content: `I found no tracks for ${query}`,
-                ephemeral: true,
-            });
-        } else {
-            await player.play(channel, searchResult, {
+
+        try {
+            const { track } = await player.play(channel, query, {
                 nodeOptions: {
                     metadata: interaction,
                 },
             });
 
-            await interaction.editReply(`Loading your track...`);
+            return interaction.followUp(`Loading track **${track.title}**...`);
+        } catch (e) {
+            return interaction.followUp(`Something went wrong: ${e}`);
         }
     },
 };
