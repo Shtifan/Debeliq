2cc01a711db654311addc59c13f6c85ec3e9f6f2
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-01-02 14:30:15 +0200

    added seek command

--- src/index.js
+++ src/index.js
@@ -49,11 +49,11 @@ client.on('interactionCreate', async interaction => {
         await command.execute(interaction);
     } catch (error) {
         console.error(error);
-        await interaction.deferReply();
-        return interaction.editReply({
-            content: 'There was an error while executing this command',
-            ephemeral: true,
-        });
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
     }
 });
 
