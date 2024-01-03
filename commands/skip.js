6bdab2a3af871944045e9796080c0ddd973a756b
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-01-03 22:15:12 +0200

    tab size change

--- commands/skip.js
+++ commands/skip.js
@@ -2,25 +2,25 @@ const { SlashCommandBuilder } = require("discord.js");
 const { useQueue } = require("discord-player");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("skip").setDescription("Plays next song in queue"),
+    data: new SlashCommandBuilder().setName("skip").setDescription("Plays next song in queue"),
 
-  async execute(interaction) {
-    const channel = interaction.member.voice.channel;
-    if (!channel)
-      return interaction.reply({
-        content: "You are not connected to a voice channel",
-        ephemeral: true,
-      });
+    async execute(interaction) {
+        const channel = interaction.member.voice.channel;
+        if (!channel)
+            return interaction.reply({
+                content: "You are not connected to a voice channel",
+                ephemeral: true,
+            });
 
-    const queue = useQueue(interaction.guild.id);
-    if (!queue || !queue.node.isPlaying())
-      return interaction.reply({
-        content: `There is no music currently playing`,
-        ephemeral: true,
-      });
+        const queue = useQueue(interaction.guild.id);
+        if (!queue || !queue.node.isPlaying())
+            return interaction.reply({
+                content: `There is no music currently playing`,
+                ephemeral: true,
+            });
 
-    queue.node.skip();
+        queue.node.skip();
 
-    return interaction.reply(`Skipping **${queue.currentTrack.title}**`);
-  },
+        return interaction.reply(`Skipping **${queue.currentTrack.title}**`);
+    },
 };
