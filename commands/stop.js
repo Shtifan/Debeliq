cf4a9e90fa1cd84474755879afe9af465a3ff488
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-01-03 19:52:39 +0200

    .

--- commands/stop.js
+++ commands/stop.js
@@ -1,26 +1,26 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { useQueue } = require('discord-player');
+const { SlashCommandBuilder } = require("discord.js");
+const { useQueue } = require("discord-player");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('stop').setDescription('Stops the music'),
+    data: new SlashCommandBuilder().setName("stop").setDescription("Stops the music"),
 
-  async execute(interaction) {
-    const channel = interaction.member.voice.channel;
-    if (!channel)
-      return interaction.reply({
-        content: 'You are not connected to a voice channel',
-        ephemeral: true,
-      });
+    async execute(interaction) {
+        const channel = interaction.member.voice.channel;
+        if (!channel)
+            return interaction.reply({
+                content: "You are not connected to a voice channel",
+                ephemeral: true,
+            });
 
-    const queue = useQueue(interaction.guild.id);
-    if (!queue || !queue.node.isPlaying())
-      return interaction.reply({
-        content: `There is no music currently playing`,
-        ephemeral: true,
-      });
+        const queue = useQueue(interaction.guild.id);
+        if (!queue || !queue.node.isPlaying())
+            return interaction.reply({
+                content: `There is no music currently playing`,
+                ephemeral: true,
+            });
 
-    queue.delete();
+        queue.delete();
 
-    return interaction.reply('Music stopped in the server, see you next time');
-  },
+        return interaction.reply("Music stopped in the server, see you next time");
+    },
 };
