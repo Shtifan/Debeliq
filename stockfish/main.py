3fc77baaedf8035ffd8d0ccefd8c0aed0d5bf058
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-01-10 19:31:11 +0200

    updated chess solver

--- stockfish/main.py
+++ stockfish/main.py
@@ -1,6 +1,8 @@
 from stockfish import Stockfish
 
-# def get_fen(image):
+
+def get_fen(image_path):
+    return fen
 
 
 def get_best_move(stockfish, fen):
@@ -9,24 +11,19 @@ def get_best_move(stockfish, fen):
     return best_move
 
 
-def main():
-    # fen=get_fen('./stockfish/board.png')
-    fen = "r5k1/1bq1br2/ppn2n1Q/2ppp3/P2P4/2P2NN1/1P2BPPP/R4RK1"
+image_path = "./stockfish/board.png"
+fen = get_fen(image_path)
 
-    stockfish = Stockfish("./stockfish/stockfish.exe")
+stockfish = Stockfish("C:/Stockfish/stockfish.exe")
 
-    try:
-        best_move_white = get_best_move(stockfish, fen + " w - - 0 1")
-        best_move_black = get_best_move(stockfish, fen + " b - - 0 1")
+try:
+    best_move_white = get_best_move(stockfish, fen + " w - - 0 1")
+    best_move_black = get_best_move(stockfish, fen + " b - - 0 1")
 
-        with open("./stockfish/best_moves.txt", "w") as file:
-            file.write(f"{best_move_white}\n")
-            file.write(f"{best_move_black}\n")
+    with open("./stockfish/best_moves.txt", "w") as file:
+        file.write(f"{best_move_white}\n")
+        file.write(f"{best_move_black}\n")
 
-    except Exception as e:
-        with open("./stockfish/best_moves.txt", "w") as file:
-            pass
-
-
-if __name__ == "__main__":
-    main()
+except Exception as e:
+    with open("./stockfish/best_moves.txt", "w") as file:
+        pass
