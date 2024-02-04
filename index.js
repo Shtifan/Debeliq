6e99e8de945cf9db537bba1813bddce2fdf853ca
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-02-04 22:26:10 +0200

    fixed command deployment

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ],
 });
+
 module.exports = client;
 
 client.commands = new Collection();
+const commands = [];
+
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -26,12 +29,18 @@ for (const folder of commandFolders) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
         if ("data" in command && "execute" in command) {
+            commands.push(command.data.toJSON());
             client.commands.set(command.data.name, command);
         } else {
             console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
     }
 }
+
+client.once("ready", (client) => {
+    client.application.commands.set(commands);
+    console.log(`Ready on ${client.guilds.cache.size} server${client.guilds.cache.size != 1 ? "s" : ""}!`);
+});
 
 const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
