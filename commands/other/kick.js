45ab13715154e82fe3d63c9be8b9580ef385db7f
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-02-05 11:11:59 +0200

    added kick timeout ban cmds

---
+++ commands/other/kick.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("kick")
+        .setDescription("Kicks a member from this server")
+        .addUserOption((option) => option.setName("user").setDescription("The user you want to kick").setRequired(true))
+        .addStringOption((option) => option.setName("reason").setDescription("The reason for the kick")),
+
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        const targetUserId = interaction.options.getUser("user");
+        const targetUser = await interaction.guild.members.fetch(targetUserId);
+
+        if (!targetUser) {
+            await interaction.reply({
+                content: "That user doesn't exist in this server.",
+                ephemeral: true,
+            });
+            return;
+        }
+
+        let reason = interaction.options.getString("reason");
+        if (!reason) reason = "nigger";
+
+        try {
+            await targetUser.kick({ reason });
+            await interaction.editReply(`User ${targetUser} was kicked\nReason: ${reason}`);
+        } catch (error) {
+            await interaction.reply({
+                content: `Sorry I am unable to kick ${targetUser}`,
+                ephemeral: true,
+            });
+        }
+    },
+};
---
+++ commands/other/timeout.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require("discord.js");
+const ms = require("ms");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("timeout")
+        .setDescription("Timeouts a member from this server")
+        .addUserOption((option) => option.setName("user").setDescription("The user you want to timeout").setRequired(true))
+        .addStringOption((option) =>
+            option.setName("duration").setDescription("Timeout duration (10s, 30m, 1h, 5 day...)").setRequired(true)
+        )
+        .addStringOption((option) => option.setName("reason").setDescription("The reason for the timeout")),
+
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        const targetUserId = interaction.options.getUser("user");
+        const targetUser = await interaction.guild.members.fetch(targetUserId);
+
+        if (!targetUser) {
+            await interaction.reply({
+                content: "That user doesn't exist in this server.",
+                ephemeral: true,
+            });
+            return;
+        }
+
+        const duration = interaction.options.getString("duration");
+        const msDuration = ms(duration);
+
+        let reason = interaction.options.getString("reason");
+        if (!reason) reason = "nigger";
+
+        try {
+            await targetUser.timeout(msDuration, reason);
+            await interaction.editReply(`${targetUser} was timed out for ${duration}.\nReason: ${reason}`);
+        } catch (error) {
+            await interaction.reply({
+                content: `Sorry I am unable to timeout ${targetUser}`,
+                ephemeral: true,
+            });
+        }
+    },
+};
