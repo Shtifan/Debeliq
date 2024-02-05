e2d0aa355a107b399cdf0238fc9d834ea691eb69
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-02-05 09:41:31 +0200

    added ping cmd

--- commands/other/ping.js
+++ commands/other/ping.js
@@ -1,7 +1,15 @@
 const { SlashCommandBuilder } = require("discord.js");
+const client = require("../../index.js");
 
 module.exports = {
-    data: new SlashCommandBuilder().setName("ping").setDescription("Replies with bot ping in ms"),
+    data: new SlashCommandBuilder().setName("ping").setDescription("Replies with bot latency in ms"),
 
-    async execute(interaction) {},
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        const reply = await interaction.fetchReply();
+        const ping = reply.createdTimestamp - interaction.createdTimestamp;
+
+        await interaction.editReply(`${ping}ms`);
+    },
 };
