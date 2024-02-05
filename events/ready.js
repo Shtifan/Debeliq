e908e49ea48a0c465efb0e2143cdfd3d6d2c1dea
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-02-05 14:40:39 +0200

    .

--- events/ready.js
+++ events/ready.js
@@ -4,10 +4,6 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        console.log(
-            `Ready! Logged in as ${client.user.tag} on ${client.guilds.cache.size} server${
-                client.guilds.cache.size != 1 ? "s" : ""
-            }!`
-        );
+        console.log(`Ready on ${client.guilds.cache.size} servers!`);
     },
 };
