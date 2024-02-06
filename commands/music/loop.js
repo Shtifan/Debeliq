6212f2baf03227afc576ed8d53e99217761f613e
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-02-06 10:04:20 +0200

    switch statement

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -11,9 +11,10 @@ module.exports = {
                 .setDescription("What action you want to preform on the loop")
                 .setRequired(true)
                 .addChoices(
-                    { name: "Song", value: "song" },
-                    { name: "Queue", value: "queue" },
-                    { name: "Disabled", value: "off" }
+                    { name: "Off", value: "0" },
+                    { name: "Track", value: "1" },
+                    { name: "Queue", value: "2" },
+                    { name: "Autoplay", value: "3" }
                 )
         ),
 
@@ -37,24 +38,27 @@ module.exports = {
         }
 
         switch (interaction.options.getString("action")) {
-            case "song": {
-                queue.setRepeatMode(QueueRepeatMode.TRACK);
-
-                await interaction.reply(
-                    "Repeat mode **enabled**. The current song will be repeated endlessly (you can end the loop with **/loop Disable**)."
-                );
-            }
-            case "queue": {
-                queue.setRepeatMode(QueueRepeatMode.QUEUE);
-
-                await interaction.reply(
-                    "Repeat mode **enabled** for the whole queue. It will be repeated endlessly (you can end the loop with **/loop Disable**)."
-                );
-            }
-            case "off": {
+            case "0": {
                 queue.setRepeatMode(QueueRepeatMode.OFF);
-
                 await interaction.reply("Repeat mode **disabled**.");
+                break;
+            }
+            case "1": {
+                queue.setRepeatMode(QueueRepeatMode.TRACK);
+                await interaction.reply("Repeat mode **enabled**.");
+                break;
+            }
+            case "2": {
+                queue.setRepeatMode(QueueRepeatMode.QUEUE);
+                await interaction.reply("Repeat mode **enabled** for the whole queue.");
+                break;
+            }
+            case "3": {
+                queue.setRepeatMode(QueueRepeatMode.AUTOPLAY);
+                await interaction.reply(
+                    "The player will now play related songs automatically based on your existing queue."
+                );
+                break;
             }
         }
     },
