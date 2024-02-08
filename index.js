1ee56f373724ed56aed6f2c1c1d7f03b492cd13d
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-02-08 10:37:52 +0200

    .

--- index.js
+++ index.js
@@ -17,15 +17,14 @@ const client = new Client({
 module.exports = client;
 
 client.commands = new Collection();
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
+const commandsPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(commandsPath);
 
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+    const commandFiles = fs.readdirSync(path.join(commandsPath, folder)).filter((file) => file.endsWith(".js"));
 
     for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
+        const filePath = path.join(path.join(commandsPath, folder, file));
         const command = require(filePath);
 
         if ("data" in command && "execute" in command) {
