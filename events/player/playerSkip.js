8a2e110c842b29d723de2470d4a1cad5516fbce8
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-08-06 16:30:43 +0300

    fixed some player events and commands

--- events/player/playerSkip.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "playerSkip",
-    type: "player",
-    once: false,
-    execute(queue, track) {
-        queue.metadata.send(`Skipping **${track.title}** due to an issue!`);
-    },
-};
