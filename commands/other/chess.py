8fb2d4d610b05e18dd8455292f15eb0e0a5994d8
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2024-08-26 19:50:35 +0300

    fixed chess solver command

--- commands/other/chess.py
+++ commands/other/chess.py
@@ -4,10 +4,41 @@ import sys
 import cv2
 import numpy as np
 
-def get_best_move(stockfish, fen):
-    stockfish.set_fen_position(fen)
-    best_move = stockfish.get_best_move()
-    return best_move
+image_path = "./data/image.png"
+stockfish = Stockfish("./commands/other/stockfish.exe")
+
+def crop_chessboard(image_path):
+    img = cv2.imread(image_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    edges = cv2.Canny(blur, 50, 150)
+
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    for contour in contours:
+        peri = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
+
+        if len(approx) == 4:
+            x, y, w, h = cv2.boundingRect(approx)
+
+            cropped_img = img[y:y + h, x:x + w]
+
+            cv2.imwrite(image_path, cropped_img)
+            return
+
+crop_chessboard(image_path)
+
+fen = get_fen_from_image_path(image_path)
+if not stockfish.is_fen_valid(fen + " w - - 0 1"):
+    sys.exit()
+
+with open("./data/move.txt", "r") as file:
+    move_color = file.read().strip()
 
 def rotate_fen(fen):
     fen_parts = fen.split('/')
@@ -15,22 +46,13 @@ def rotate_fen(fen):
     rotated_fen = '/'.join(reversed_fen_parts)
     return rotated_fen
 
-input_path = "./data/image.png"
-output_path = "./data/output.png"
-stockfish = Stockfish("./commands/other/stockfish.exe")
+if move_color == 'b':
+    fen = rotate_fen(fen)
 
-#crop_chessboard(input_path)
+fen_with_color = fen + f" {move_color} - - 0 1"
 
-fen = get_fen_from_image_path(input_path)
-if not stockfish.is_fen_valid(fen + " w - - 0 1"):
-    sys.exit()
+stockfish.set_fen_position(fen_with_color)
 
-best_move_white = get_best_move(stockfish, fen + " w - - 0 1")
-print(best_move_white)
+best_move = stockfish.get_best_move()
 
-fen = rotate_fen(fen)
-
-best_move_black = get_best_move(stockfish, fen + " b - - 0 1")
-print(best_move_black)
-
-#draw_moves(input_path,fen,best_move_black,output_path)
+print(best_move)
