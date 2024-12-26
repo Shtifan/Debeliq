ca278acb6758adcee90a6c211c895af2cfb38e24
stefan <sstoyanov.stefan@gmail.com>
2024-12-26 16:30:31 +0200

    .

--- commands/other/koj.js
+++ commands/other/koj.js
@@ -12,5 +12,5 @@ module.exports = {
 client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
 
-    if (message.content.includes("koi") || message.content.includes("koj")) await message.reply("te e pital");
+    if (message.content.includes("koj")) await message.reply("te e pital");
 });
