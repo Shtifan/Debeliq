6ab261ddd710261ed201cd5528c1a5d9dd2a8e1f
stefan <sstoyanov.stefan@gmail.com>
2025-01-03 16:31:32 +0200

    implement volume command to set and check player's volume in music queue

---
+++ commands/music/volume.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { useQueue } = require("discord-player");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("volume")
+        .setDescription("Set or check the player's volume")
+        .addIntegerOption((option) => option.setName("value").setDescription("Volume level").setRequired(false)),
+
+    async execute(interaction) {
+        const queue = useQueue(interaction.guild.id);
+
+        if (!queue || !queue.currentTrack) {
+            await interaction.reply({
+                content: "No song is currently playing.",
+                ephemeral: true,
+            });
+            return;
+        }
+
+        const volumeValue = interaction.options.getInteger("value");
+
+        if (volumeValue !== null) {
+            queue.node.setVolume(volumeValue);
+            await interaction.reply(`Volume has been set to ${volumeValue}%.`);
+        } else {
+            const currentVolume = queue.node.volume;
+            await interaction.reply(`The current volume is ${currentVolume}%.`);
+        }
+    },
+};
