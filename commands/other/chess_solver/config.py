c297f65e7c29dbef10d6c305744767894097131b
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2025-04-19 13:38:55 +0300

    refactor: streamline image handling in chess solver and update config paths

--- commands/other/chess_solver/config.py
+++ commands/other/chess_solver/config.py
@@ -3,6 +3,6 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(ROOT_DIR, "saved_models")
 STOCKFISH_PATH = "stockfish"
-IMAGE_PATH = r"C:\Users\sstoy\Documents\Programing\Projects\Debeliq\data\chessboard.png"
-OUTPUT_PATH = r"solved_chess_inverted.png"
+IMAGE_PATH = r"chessboard.png"
+OUTPUT_PATH = r"solved_chessboard_inverted.png"
 INVERT_COLORS = True
