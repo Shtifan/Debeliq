9439587fea1a42bbbc1043fbd755e124e9f5f180
Stefan Stoyanov <sstoyanov.stefan@gmail.com>
2025-07-15 13:39:46 +0300

    removed music

--- events/player/emptyChannel.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "emptyChannel",
-    type: "player",
-    once: false,
-    execute(queue) {
-        queue.metadata.channel.send(`Leaving because there is no one in the voice channel.`);
-    },
-};
